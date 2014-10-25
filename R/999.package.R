087ec20ad49e04f955855eec7453c4d4db299c51
H Bengtsson <hb@aroma-project.org>
2014-10-25 00:53:38 +0000

    Rebuild Rd
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/AffymetrixDataTestFiles@3080 db2202da-8704-0410-b924-b125b3cd84ef

--- R/999.package.R
+++ R/999.package.R
@@ -8,14 +8,14 @@
 #
 # \section{Requirements}{
 #   None.
-# } 
+# }
 #
 # \section{Installation}{
 #   To install this package, see \url{http://www.bioconductor.org/}.
 # }
 #
 # \section{Location of data files}{
-#   Data files are located in two different directory tree depending if
+#   Data files are located in different directory tree depending if
 #   they are annotation data files or sample data files.
 #   The roots of these two directory structures are:
 #   \preformatted{
@@ -27,21 +27,18 @@
 #
 #   Directory structure:
 # \preformatted{
-# @eval "library(R.utils); path <- system.file(package='AffymetrixDataTestFiles'); owd <- setwd(path); x <- unlist(sapply(c('annotationData', 'rawData'), function(path) { paste(path, list.files(path=path, recursive=TRUE), sep='/')})); setwd(owd); x <- paste(toFileListTree(x, dirsFirst=FALSE), collapse='\n'); x;"
+# @eval "local({ path <- system.file(package='AffymetrixDataTestFiles'); opwd <- setwd(path); on.exit(setwd(opwd)); library(R.utils); x <- unlist(sapply(c('annotationData', 'rawData'), function(path) { paste(path, list.files(path=path, recursive=TRUE), sep='/')})); x <- toFileListTree(x, dirsFirst=FALSE); x <- pasteTree(x); x <- paste(x, collapse='\n'); x })"
 # }
 # }
 #
 # \author{
-#  Adopted from [1] by
-#   Henrik Bengtsson, \email{hb@stat.berkeley.edu},
-#   James Bullard, \email{bullard@stat.berkeley.edu} and 
-#   Kasper Daniel Hansen, \email{khansen@stat.berkeley.edu}.
+#  Adopted from [1] by the authors of this package.
 # }
 #
 # \section{License}{
 #   LGPL version 2.1 according to [1].
 # }
-# 
+#
 # \references{
 #   [1] Affymetrix Inc, Fusion Software Developers Kit (SDK), 2007.
 #       \url{http://www.affymetrix.com/support/developer/fusion/}\cr
--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,5 @@
-## .onAttach <- function(libname, pkgname) {
-##   dirs <- c("annotationData", "rawData");
-##   paths <- lapply(dirs, FUN=function(dir) {
-##     system.file(dir, package=pkgname);
-##   })
-##   names(paths) <- dirs;
-## 
-##   pos <- grep(sprintf("^package:%s$", pkgname), search());
-##   assign(pkgname, paths, pos=pos);
-## }
-
 .onLoad <- function(libname, pkgname) {
-#  cat(sprintf(".onLoad('%s','%s') called\n", libname, pkgname));
-
-  # Append package's annotationData/ directory tree to the 
+  # Append package's annotationData/ directory tree to the
   # search path of affxparser::findCdf().
   paths <- getOption("AFFX_CDF_PATH");
   if (length(paths) > 0) {
@@ -26,28 +13,3 @@
   paths <- paste(unique(paths), collapse=";");
   options("AFFX_CDF_PATH"=paths);
 }
-
-## .Last.lib <- function(libpath) {
-##   cat(sprintf(".Last.lib('%s') called\n", libpath));
-## }
-
-## .onUnload <- function(libpath) {
-## #  cat(sprintf(".onLoad('%s') called\n", libpath));
-## 
-##   # Figure out the package name (HOW?!?)
-##   pkgname <- basename(libname);
-##   # Handle package version number
-##   pkgname <- gsub("_[0-9.]*$", "", pkgname);
-## 
-##   # Remove the package's annotationData/ directory tree from the 
-##   # search path of affxparser::findCdf().
-##   paths <- getOption("AFFX_CDF_PATH");
-##   paths <- unlist(strsplit(paths, split=";"), use.names=FALSE);
-##   paths <- paths[nchar(paths) > 0];
-## 
-##   path <- system.file("annotationData", package=pkgname);
-##   paths <- setdiff(paths, path);
-## 
-##   paths <- paste(unique(paths), collapse=";");
-##   options("AFFX_CDF_PATH"=paths);
-## }
